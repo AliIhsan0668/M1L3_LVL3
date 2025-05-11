2ba8235b17cc71f45ef2302591bd776425d29988
AliIhsan0668 <aliihsancoskun6806@gmail.com>
2025-05-11 22:39:44 +0300

    Sohbet odasına yeni bir kullanıcı eklendiğinde tepki veren bot ve tahmin botu eklendi

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import discord
+import discord,random,asyncio
 from discord.ext import commands
 from config import token  # Botun tokenini config dosyasından içe aktarma
 
@@ -49,4 +49,29 @@ async def on_message(message):
 async def on_message(message):
     await message.channel.send(message.content)
 
+@bot.event
+async def on_member_join(member):
+    # Karşılama mesajı gönderme
+    for channel in member.guild.text_channels:
+        await channel.send(f'Hoş geldiniz: , {member.mention}!')
+                           
+@bot.event
+async def guess(message,self):
+    if message.author==bot.user:
+        return
+    if message.content.startswith('$guess'):
+        await message.channel.send('1 ve 10 arasında sayı tuttum bil bakalım!')
+        def is_correct(m):
+            return m.author == message.author and m.content.isdigit()
+
+        answer = random.randint(1, 10)
+
+        try:
+            guess = await self.wait_for('message', check=is_correct, timeout=5.0)
+        except asyncio.TimeoutError:
+            return await message.channel.send(f'Üzgünüm,cevap vermen çok uzun sürdü {answer}.')
+        if int(guess.content) == answer:
+            await message.channel.send('Doğru!')
+        else:
+            await message.channel.send(f'Hayır,Sayı = {answer}.')
 bot.run(token)
